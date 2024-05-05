c44d311951c37283def2250b4ed3c1f9947b2d33
svenkuhn <kuhn@holsterlabs.de>
2024-05-05 07:27:39 +0200

    add title_html option add description_html option add definition_html option

--- src/Widget/ListWidget.php
+++ src/Widget/ListWidget.php
@@ -9,28 +9,40 @@ class ListWidget extends AbstractWidget
     public function configureOptions(OptionsResolver $resolver)
     {
         $this->configureDefaultOptions($resolver);
+
+        $resolver->define('title_html')
+            ->allowedTypes('bool')
+            ->default(false);
+
+        $resolver->define('description_html')
+            ->allowedTypes('bool')
+            ->default(false);
+
+        $resolver->define('definition_html')
+            ->allowedTypes('bool')
+            ->default(false);
+
         $resolver->define('list')
-        ->required()
-        ->allowedTypes('null', 'array[]')
-        ->allowedValues(static function (array &$lines): bool {
-            $subResolver = new OptionsResolver();
-            $subResolver->define('href')
-                ->allowedTypes('string');
+            ->required()
+            ->allowedTypes('null', 'array[]')
+            ->allowedValues(static function (array &$lines): bool {
+                $subResolver = new OptionsResolver();
+                $subResolver->define('href')
+                    ->allowedTypes('string');
 
-            $subResolver->define('title')
-                ->required()
-                ->allowedTypes('string');
+                $subResolver->define('title')
+                    ->required()
+                    ->allowedTypes('string');
 
-            $subResolver->define('description')
-                ->allowedTypes('string');
+                $subResolver->define('description')
+                    ->allowedTypes('string');
 
-            $subResolver->define('definition')
-            ->allowedTypes('string');
+                $subResolver->define('definition')
+                    ->allowedTypes('string');
 
-
-            $lines = array_map([$subResolver, 'resolve'], $lines);
-            return true;
-        });
+                $lines = array_map([$subResolver, 'resolve'], $lines);
+                return true;
+            });
     }
 
     public function getTemplatePath(): string
