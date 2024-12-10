d73f99a773d3aca672d9d8abd16fd9daf3a4b245
Sven Kuhn <kuhn@holsterlabs.de>
2024-12-10 12:30:51 +0100

    add html to listWidget

--- src/Widget/ListWidget.php
+++ src/Widget/ListWidget.php
@@ -9,24 +9,34 @@ class ListWidget extends AbstractWidget
     public function configureOptions(OptionsResolver $resolver)
     {
         $this->configureDefaultOptions($resolver);
-        $resolver->define('list')
-        ->required()
-        ->allowedTypes('null', 'array[]')
-        ->allowedValues(static function (array &$lines): bool {
-            $subResolver = new OptionsResolver();
-            $subResolver->define('href')
-                ->allowedTypes('string');
+        $resolver
+            ->define('title_html')
+            ->allowedTypes('bool')
+            ->define('description_html')
+            ->allowedTypes('bool')
+            ->define('definition_html')
+            ->allowedTypes('bool')
+            ->define('list')
+            ->required()
+            ->allowedTypes('null', 'array[]')
+            ->allowedValues(static function (array &$lines): bool {
+                $subResolver = new OptionsResolver();
+                $subResolver
+                    ->define('href')
+                    ->allowedTypes('string');
 
-            $subResolver->define('title')
-                ->required()
-                ->allowedTypes('string');
+                $subResolver
+                    ->define('title')
+                    ->required()
+                    ->allowedTypes('string');
 
-            $subResolver->define('description')
-                ->allowedTypes('string');
+                $subResolver
+                    ->define('description')
+                    ->allowedTypes('string');
 
-            $subResolver->define('definition')
-            ->allowedTypes('string');
-
+                $subResolver
+                    ->define('definition')
+                    ->allowedTypes('string');
 
             $lines = array_map([$subResolver, 'resolve'], $lines);
             return true;
